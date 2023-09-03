36be7b3e846fa67d54788c0c760ad1a7e1c3f51d
suno <tnsgh3691@gmail.com>
2023-09-03 21:37:50 +0900

    feat :  60주년기념관 모달창 띄우기

--- src/pages/60thPage/60thPage.jsx
+++ src/pages/60thPage/60thPage.jsx
@@ -2,15 +2,38 @@
 import React from 'react';
 import Menubar from '../../components/Menubar';
 import './60thPage.css';
+import { useState } from 'react';
 
 const SixtiethPage = () => {
+
+  let [modal, setModal] = useState(false);
+
   return (
     <article>
         <Menubar />
         {/* 아래부터 작업하면 됩니다. */}
-        60주념 기념관
+        <div className="sixth">
+          <div onClick={() => {setModal(!modal)}}className='truck'>
+            트럭
+          </div>
+          {modal === true ? <Modal/> : null}
+        </div>
     </article>
+
   );
 };
+
+const Modal = () => {
+
+  return (
+    <div className='modal'>
+      <div>음식 종류</div>
+      <div>가격</div>
+      <div>기타 등등</div>
+    </div>
+  )
+}
+
+
 
 export default SixtiethPage;
