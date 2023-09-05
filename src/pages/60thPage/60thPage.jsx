6b1695d804aae1e5d49a1c847294b27619a74e7b
suno <tnsgh3691@gmail.com>
2023-09-05 21:35:38 +0900

    feat : 모달창 빠져나오기 버튼 + 주점 리스트 전체보기 버튼과 모달창 생성

--- src/pages/60thPage/60thPage.jsx
+++ src/pages/60thPage/60thPage.jsx
@@ -13,23 +13,45 @@ const SixtiethPage = () => {
         <Menubar />
         {/* 아래부터 작업하면 됩니다. */}
         <div className="sixth">
-          <div onClick={() => {setModal(!modal)}}className='truck'>
+          <div onClick={() => {setModal(!modal)}} className='truck'>
             트럭
           </div>
-          {modal === true ? <Modal/> : null}
+          {modal === true ? <Modal setModal={setModal} /> : null}
         </div>
     </article>
 
   );
 };
 
-const Modal = () => {
+const Modal = (props) => {
+
+  let [pub,setPub] = useState(false);
 
   return (
+    <>
     <div className='modal'>
+      <span onClick={() => { props.setModal(false) }} className='cancel'><b> &#8678; </b></span>
+
       <div>음식 종류</div>
       <div>가격</div>
       <div>기타 등등</div>
+
+      <div onClick={() => {setPub(!pub)}} className='list'>리스트 전체보기</div>
+      {pub === true ? <Pub setPub={setPub} /> : null}
+    </div>
+    </>
+  )
+}
+
+const Pub = (props) => {
+
+  return (
+    <div className='pub'>
+      <span onClick={() => { props.setPub(false) }} className='cancel'><b> &#8678; </b></span>
+
+      <div>주점 ~ </div>
+      <div>주점 ~ </div>
+      <div>주점 ~ </div>
     </div>
   )
 }
