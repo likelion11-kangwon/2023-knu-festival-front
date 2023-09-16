f0dbaa01b7ebeb9bf5e21bd92fbb2628f479b15e
mango606 <yuj08184@kangwon.ac.kr>
2023-09-16 22:38:11 +0900

    [Fix] axios 수정

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,15 +1,12 @@
-import axios from "axios";
+import { Axios } from "axios";
 import React, { createContext, useContext } from "react";
 
-const baseURL = process.env.REACT_APP_API_URL || "http://localhost:3000";
-
-const axiosInstance = axios.create({
-  baseURL,
-  transformResponse: [data => JSON.parse(data)]
-});
+const baseURL = process.env.REACT_APP_API_URL
+  ? process.env.REACT_APP_API_URL
+  : "http://localhost:3000";
 
 /**
- * @type React.Context
+ * @type React.Context<Axios>
  */
 const axiosContext = createContext();
 
@@ -19,7 +16,9 @@ export function useAxios() {
 
 export const AxiosProvider = ({ children }) => {
   return (
-    <axiosContext.Provider value={axiosInstance}>
+    <axiosContext.Provider
+      value={new Axios({ baseURL, transformResponse: JSON.parse })}
+    >
       {children}
     </axiosContext.Provider>
   );
