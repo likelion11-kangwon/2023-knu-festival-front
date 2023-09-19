86dfbae811ad012b725f83ffa8cc009de7386678
mango606 <yuj08184@kangwon.ac.kr>
2023-09-19 13:09:22 +0900

    [Fix] useEffet 삭제

--- src/pages/60thPage/60thPage.jsx
+++ src/pages/60thPage/60thPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Modal from 'react-modal';
 import Menubar from '../../components/Menubar';
 import './60thPage.css';
